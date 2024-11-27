f5cfbf029259b478455d5b4a9871bf2962503fed
Kevin Mändmets <mandmetskevin@gmail.com>
2024-11-27 09:11:24 +0200

    - update angular 17 -> 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,18 @@ import { PostsListComponent } from './components/posts-list/posts-list.component
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatCard} from "@angular/material/card";
 import {MaterialModule} from "./material/material.module";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    PostsListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MaterialModule,
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideAnimationsAsync(),
-
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        PostsListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        MaterialModule,
+        CommonModule], providers: [
+        provideAnimationsAsync(),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
